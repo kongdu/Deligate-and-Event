d187e63d1fc0cda80450407d29fa4d19a6c5c0f8
yeon <yeonyya@gmail.com>
2019-01-21 17:08:14 +0900

    6까지 업데이트

---
+++ 2_UsingCallback/2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace UsingCallback
+{
+    internal delegate int Compare(int a, int b);
+
+    internal class MainApp
+    {
+        private static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        private static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        private static void BubbleSort(int[] DataSet, Compare myCompare)
+        {
+            int temp = 0;
+            for (int i = 0; i < DataSet.Length - 1; i++)
+            {
+                for (int j = 0; j < DataSet.Length - (i + 1); j++)
+                {
+                    if (myCompare(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 2, 10 };
+            Console.WriteLine("오름차순 정렬...");
+
+            BubbleSort(array, new Compare(AscendCompare));
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+
+            int[] array2 = { 7, 2, 8, 10, 11 };
+            Console.WriteLine("\n내림차순 정렬...");
+
+            BubbleSort(array2, new Compare(AscendCompare));
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write("{0} ", array2[i]);
+            }
+        }
+    }
+}
---
+++ 6_EventTest/6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EventTest
+{
+    internal delegate void EventHandler(string msg); // 대리자선언
+
+    internal class MyNotifier
+    {
+        public event EventHandler SomethingHappened;   //event 키워드 한정자를 붙여서 SomethingHappend라는 이벤트 선언
+
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+            if (temp != 0 && temp % 3 == 0)  //숫자가 3, 6, 9로 끝나는 값이 될때마다 이벤트가 발생하는
+            {                               //SomethingHappend 이벤트에서 사용할 이벤트
+                                            //EventHandler 대리자 형식과 동일한 메소드 형태로 구현
+                SomethingHappened(String.Format("{0} : 짝", number));
+            }
+        }
+    }
+
+    internal class MainApp
+    {
+        static public void MyHandler(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void Main(string[] args)
+        {
+            MyNotifier notifier = new MyNotifier();
+            notifier.SomethingHappened += new EventHandler(MyHandler); //작성한 이벤트 핸들러 등록
+            //notifier.SomethingHappened += MyHandler;
+
+            for (int i = 0; i < 30; i++)
+            {
+                notifier.DoSomething(i);
+                //DoSomething을 호출하면 그 안에있던 SomethingHappened라는 이벤트 발생하고. 그럴때마다 MyHandler가 처리
+            }
+        }
+    }
+}
